4235432a09bad7bb2bcdf26718381705fd0dec2f
Siobhan Phelan <b00139195@mytudublin.ie>
2022-02-14 17:57:00 +0000

    Added try exceot block for testing

--- camera.py
+++ camera.py
@@ -10,29 +10,37 @@ camera = PiCamera()
 # function to capture still image and save to file with sequential numbering
 def takephoto():
     image = 1               # set file count to 1
-    camera.start_preview()  # start preview mode to adjust to light settings
-    sleep(3)                # wait 3 seconds
+    sleep(2)                # wait 2 seconds
     camera.capture(
-        '/home/sio/testimages/Catcam/test%03d.jpg' % image)  # take photo; save to file path
+        '/home/sio/Catcam/testimages/test%03d.jpg' % image)  # take photo; save to file path
     image += 1              # increment file count
-    camera.stop_preview()   # end preview mode
 
 
 # function to capture 10-second video and save to file with sequential numbering
 def takevideo():
     video = 1               # set file count to 1
-    camera.start_preview()  # start preview mode to adjust to light settings
     camera.start_recording(
-        '/home/sio/testvideos/Catcam/test%03d.h264' % video)  # begin video capture; save to file path
-    sleep(10)               # wait 10 seconds
+        '/home/sio/Catcam/testvideos/test%03d.h264' % video)  # begin video capture; save to file path
+    sleep(5)               # wait 5 seconds
     camera.stop_recording() # stop video capture
     video += 1              # increment file count
-    camera.stop_preview()   # end preview mode
 
 
 # test functionality
-takephoto()
-takephoto()
+try:
+    camera.start_preview()  # start preview mode to adjust to light settings
+    takephoto()
+    sleep(3)
+    takevideo()
 
-takevideo()
-takevideo()
+except KeyboardInterrupt:
+    camera.stop_preview()   # end preview mode
+    camera.close()
+
+except:
+    camera.stop_preview()   # end preview mode
+    camera.close()
+
+finally:
+    camera.stop_preview()   # end preview mode
+    camera.close()
