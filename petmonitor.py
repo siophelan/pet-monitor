3df131ff9109929e6510e5ef15291314c0d0281b
Siobhan Phelan <b00139195@mytudublin.ie>
2022-02-15 16:44:01 +0000

    Updated code after testing

--- petmonitor.py
+++ petmonitor.py
@@ -2,34 +2,69 @@
 
 from gpiozero import MotionSensor
 from picamera import PiCamera
-from time import sleep
+from time import sleep, gmtime, strftime
 
 pir = MotionSensor(17)
 camera = PiCamera()
-# optional: set camera.resolution and/or camera.framerate
-# optional: configure other camera settings, e.g. camera.awb_mode; camera.exposure_mode; camera.ISO
+camera.hflip = True
 
-try:
+def main():
     print("Testing motion capture (enter CTRL+C to end)")
 
-    # wait for signal from PIR sensor
     while True:
         pir.wait_for_motion()
+
+        # on signal from PIR sensor
         print("Movement detected!")
         camera.start_preview()
+        
+        # set variables
+        global imgfilepath, vidfilepath
+        timestamp = strftime("%y%m%d_%H%M%S", gmtime())
+        imgfilepath = (
+            "/home/sio/Catcam/testimages/img_{timestamp}.jpg".format(timestamp=timestamp))
+        vidfilepath = (
+            "/home/sio/Catcam/testvideos/vid_{timestamp}.h264".format(timestamp=timestamp))
+        
+        # STILL TO FIGURE OUT:
+        # if time elapsed between timestamps is > 5 mins, then:
+        # take photo and record video
+        # else continue
+
+        take_photo()
+        
+        record_video()
+
         pir.wait_for_no_motion()
         camera.stop_preview()
 
-except KeyboardInterrupt:
-    # if program interrupted by CTRL+C key press
-    print("Test ended!")
-    camera.stop_preview()
+# function to capture still image with timestamped filename
+def take_photo():
+    sleep(2)    # 2 second delay
+    camera.capture(imgfilepath)
+    print("Photo taken!")
 
-except:
-    # for all other errors
-    print("An error or exception occurred!")
-    camera.stop_preview()
+# function to capture 10-second video with timestamped filename
+def record_video():
+    camera.start_recording(vidfilepath)
+    sleep(10)   # 10 second clip
+    camera.stop_recording()
+    print("Video recorded!")
 
-finally:
-    camera.stop_preview()
-    camera.close()
+# code to execute
+if __name__ == "__main__":
+    try:
+        main()
+
+    except KeyboardInterrupt:
+        # if program interrupted by CTRL+C key press
+        print("User exited program!")
+        camera.close()
+
+    except:
+        # for all other errors
+        print("An error or exception occurred!")
+        camera.close()
+
+    finally:
+        camera.close()
