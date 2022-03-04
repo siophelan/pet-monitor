d49b0a97e4aebc9fbed98b44c1f1acc72f7578ba
Siobhan Phelan <b00139195@mytudublin.ie>
2022-03-04 10:51:51 +0000

    Adjusted for sensitivity testing

--- pirsensor.py
+++ pirsensor.py
@@ -1,16 +1,21 @@
 # A script to control the PIR sensor
 
 from gpiozero import MotionSensor
+from time import sleep
+from datetime import datetime
 
 pir = MotionSensor(17)
 
 try:
     print("Testing PIR sensor (enter CTRL+C to end)")
+    sleep(2)
+    print("Ready!")
 
     # wait for signal from PIR sensor
     while True:
         pir.wait_for_motion()
-        print("Movement detected!")
+        print("Movement detected at " + str(datetime.now()))
+        sleep(5)
 
 except KeyboardInterrupt:
     # if program interrupted by CTRL+C key press
